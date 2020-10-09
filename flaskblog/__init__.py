e3b375939b53ca3ee29747c790dd1340e103377a
RNandgave <vedh.n513@gmail.com>
2020-10-09 18:09:17 +0530

    database

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # __name__ special module under python, notifies flask to look for template and static files
 # instantiation of Flask application
 app.config["SECRET_KEY"] = "dc601f91504e12a61dbb69ddd9618f86"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../site.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db = SQLAlchemy(app)
 
 from flaskblog import routes
