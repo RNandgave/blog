a299f68bc3ff42b709f1bbf910a9d538d07e9744
RNandgave <vedh.n513@gmail.com>
2020-10-13 17:16:20 +0530

    User Authentication and routing

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 # __name__ special module under python, notifies flask to look for template and static files
@@ -8,5 +9,9 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "dc601f91504e12a61dbb69ddd9618f86"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login"  # login = function name of the route
+login_manager.login_message_category = "info"
 
 from flaskblog import routes
