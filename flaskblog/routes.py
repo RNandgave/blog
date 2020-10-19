2441bb9c503dcf39f71eb75b11ec374a77e0efae
RNandgave <vedh.n513@gmail.com>
2020-10-19 17:35:23 +0530

    Pagination and user Posts

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -8,12 +8,16 @@ import os
 from PIL import Image
 
 
-@app.route("/")  # route describes addresses to different pages
+@app.route("/")
+# route describes addresses to different pages
 # '/' root page of the web application
 # to activate the debug mode set flag : FLASK_DEBUG to 1
 @app.route("/home")  # 2 routes('/' & '/home') are handled by same function
 def home():
-    posts = Post.query.all()
+    page = request.args.get("page", 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(
+        page=page, per_page=3
+    )  # breaks the total number of posts into chunks too be displayed
     return render_template("home.html", posts=posts)  # template provides the html page
 
 
@@ -164,3 +168,16 @@ def delete_post(post_id):
     db.session.commit()
     flash("Post has been deleted!", "success")
     return redirect(url_for("home"))
+
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get("page", 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = (
+        Post.query.filter_by(author=user)
+        .order_by(Post.date_posted.desc())
+        .paginate(page=page, per_page=3)
+    )  # breaks the total number of posts into chunks too be displayed
+    return render_template("user_posts.html", posts=posts, user=user)
+
